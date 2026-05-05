6b80924db9987ace066ffd058e815070d961d3a7
Thales Hasegawa <kohkihasegawa151@gmail.com>
2026-05-05 02:54:25 -0300

    feat: configura o react router para o login

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Login } from "../pages/Login";
+
+export function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
