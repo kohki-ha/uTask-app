abefe7e8e4a5b4848150dbc41103f46c39deab31
Thales Hasegawa <kohkihasegawa151@gmail.com>
2026-05-14 15:45:11 -0300

    style: ajusta estilização do componente InputField para melhor tratamento de erros

--- frontend/src/components/forms/InputField.tsx
+++ frontend/src/components/forms/InputField.tsx
@@ -32,11 +32,15 @@ export function InputField({
                     type={inputType}
                     placeholder={placeholder}
                     {...registration}
-                    className={`bg-input-bg text-text placeholder:text-label h-10 w-full rounded-lg border px-4 text-sm outline-none ${
-                        error
+                    className={`text-text placeholder:text-label h-10 w-full rounded-lg border px-4 text-sm outline-none 
+                    ${error
                             ? "border-input-error-border"
                             : "border-input-border"
-                    }`}
+                        }
+                    ${error
+                            ? "bg-input-error-bg"
+                            : "bg-input-bg"
+                        }`}
                 />
 
                 {isPassword && (
